ab97b4dc86baa62448919c2fb01674946232fca6
Bhavreet Singh <bhavreet24singh@gmail.com>
2025-10-25 00:18:58 +0530

    cross pattern day 34

--- day 34 pattern.js
+++ day 34 pattern.js
@@ -7,12 +7,27 @@ const prompt = require("prompt-sync")();
 
 const n = parseInt(prompt("Enter value : "));
 
-for (let i = n - 1; i >= 0; i--){
-    for (let j = i; j >= 1; j--){
-        process.stdout.write("  ")
+// for (let i = n - 1; i >= 0; i--){
+//     for (let j = i; j >= 1; j--){
+//         process.stdout.write("  ")
+//     }
+//     for (let j = 1; j <= n-i; j++){
+//         process.stdout.write("* ")
+//     }
+//     console.log(" ")
+// }
+
+
+
+// cross pattern
+for (let i = 1;i<=n ; i++){
+    for (let j = 1; j<=n; j++){
+        if (i == j || i + j == n + 1) {
+            process.stdout.write("* ");
+        }
+        else {
+            process.stdout.write("  "); 
+        }
     }
-    for (let j = 1; j <= n-i; j++){
-        process.stdout.write("* ")
-    }
-    console.log(" ")
+    console.log(" ");
 }
