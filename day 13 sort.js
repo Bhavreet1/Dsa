60c473d42332beac0191ef678286412a368737ce
Bhavreet Singh <bhavreet24singh@gmail.com>
2025-10-29 00:34:26 +0530

    bubble sort

--- day 13 sort.js
+++ day 13 sort.js
@@ -19,5 +19,18 @@ function selection_sort(arr) {
     }
     return arr;
 }
+// console.log(selection_sort(arr2));
 
-console.log(selection_sort(arr2));
+
+function bubble_sort(arr) {
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr.length - i; j++){
+            if (arr[j] > arr[j + 1]) {
+                [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubble_sort(arr2));
